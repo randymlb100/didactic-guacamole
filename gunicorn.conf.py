6eb1404440d3696f7a921aa215e3e329c0fa2fda
randymlb100 <randymlb100@github.com>
2026-05-09 21:35:00 -0400

    Configure Gunicorn threaded worker

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+import os
+
+bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"
+workers = int(os.environ.get("WEB_CONCURRENCY", "1"))
+worker_class = "gthread"
+threads = int(os.environ.get("GUNICORN_THREADS", "4"))
+timeout = int(os.environ.get("GUNICORN_TIMEOUT", "180"))
+graceful_timeout = int(os.environ.get("GUNICORN_GRACEFUL_TIMEOUT", "30"))
+keepalive = int(os.environ.get("GUNICORN_KEEPALIVE", "5"))
